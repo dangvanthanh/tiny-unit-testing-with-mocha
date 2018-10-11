0017007173fffc59f2bcb54f1d6666611dda47b1
Dang Van Thanh <dangvanthanh@dangthanh.org>
2018-10-11 23:32:06 +0700

    Change Promise to async/await

--- test/promise-md5.js
+++ test/promise-md5.js
@@ -3,19 +3,19 @@ var expect = require('chai').expect;
 
 describe('#promiseMd5()', function() {
   context('with string argument', function() {
-    it('should compute MD5 hash', function(done) {
-      return promiseMd5('Glad Chinda').then(function(hash) {
-        expect(hash)
-          .to.be.a('string')
-          .that.matches(/^[a-f0-9]{32}$/)
-          .and.equal('877dbb93f50eb8a89012e15bd37ee7e4');
-      });
+    it('should compute MD5 hash', async function() {
+      var hash = await promiseMd5('Glad Chinda');
+
+      expect(hash)
+        .to.be.a('string')
+        .that.matches(/^[a-f0-9]{32}$/)
+        .and.equal('877dbb93f50eb8a89012e15bd37ee7e4');
     });
   });
 
   context('with non-string argument', function() {
-    it('should throw an error', function(done) {
-      return promiseMd5(12345).catch(function(err) {
+    it('should throw an error', async function() {
+      await promiseMd5(12345).catch(function(err) {
         expect(function() {
           throw err;
         }).to.throw(
